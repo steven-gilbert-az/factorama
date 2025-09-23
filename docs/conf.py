bfefbf937d85e64b88a7ec06d45a741fe06f39ca
Steven Gilbert <steven.a.gilbert.az@gmail.com>
2025-09-22 22:51:15 -0700

    add docs

---
+++ docs/conf.py
@@ -0,0 +1,64 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+project = 'Factorama'
+copyright = '2024, Factorama Contributors'
+author = 'Factorama Contributors'
+version = '1.0.0'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'breathe',
+    'myst_parser',
+]
+
+# Breathe configuration for C++ documentation
+breathe_projects = {
+    "factorama": "../build/docs/xml"
+}
+breathe_default_project = "factorama"
+
+# Source file extensions
+source_suffix = ['.rst', '.md']
+
+# Templates path
+templates_path = ['_templates']
+
+# Exclude patterns
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for autodoc -----------------------------------------------------
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# -- Napoleon settings -------------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+
+# -- Intersphinx mapping ----------------------------------------------------
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+}
+
+# -- Python path setup ------------------------------------------------------
+sys.path.insert(0, os.path.abspath('../python_bindings/python'))
